5b33fd82278f2d94d2267b5646e92a48b5001d0b
rachmad sukri <rachmadsukri@gmail.com>
2026-05-05 22:07:48 +0700

    feat: add Docker support and configure standalone output for production deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
